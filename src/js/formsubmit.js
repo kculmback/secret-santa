b5a5d64987bdd6fc19a5960741115834cd88fd71
Kasey Culmback <kasey@kcfeatures.com>
2016-12-14 01:01:43 -0800

    Multiple updates; see description
    Replaced formToJSON function with jQuery version
    Add in better comments
    Utilize temporary predefined object in order to save time filling out
    form
    Remove console.log functions
    Assign person’s giftie to their object property
    Email everyone their giftie’s name

--- src/js/formsubmit.js
+++ src/js/formsubmit.js
@@ -8,25 +8,6 @@
 };
 
 
-
-/**
- * Retrieves input data from a form and returns it as a JSON object.
- * @param  {HTMLFormControlsCollection} elements  the form elements
- * @return {Object}                               form data as an object literal
- */
-/*function formToJSON(elements) {
-    return [].reduce.call(elements, function (data, element) {
-        if (element.name.slice(0,4) === "name") {
-        	data[element.name] = element.value;
-        }
-        
-        return data;
-    }, {});
-	
-
-};*/
-
-
 /**
  * A handler function to prevent default submission and run our custom script.
  * @param  {Event} event  the submit event triggered by the user
@@ -37,39 +18,34 @@ function handleFormSubmit(event) {
   // Stop the form from submitting since we’re handling that with AJAX.
   event.preventDefault();
   
-  // TODO: Call our function to get the form data.
-  // var data = formToJSON(form.elements);
-  /*var data = {};
-  $(form).find("input").each(function() {
+  // Create object with names and emails of people participating
+  // var data = {};
+  /*$(form).find("input").each(function() {
 	    // The selector will match buttons; if you want to filter
 	    // them out, check `this.tagName` and `this.type`; see
 	    // below
 	    // var inputType = this.tagName.toUpperCase() === "INPUT" && this.type.toUpperCase();
+	    
 	    if (this.name.slice(0,4) === "name") {
-	    	data[this.value] = $(this).next().next().next().next().val();
+	    	data[this.value] = [$(this).val(), $(this).next().next().next().next().val()]
 	    }
+	    
 	});*/
   var data = {
-  	"kasey": "kasey@test.com",
-  "jason": "jason@jason.com",
-  "tom": "tom@test.com",
-  "peter": "peter@test.com",
+  	"kasey": ["kasey", "crazyrawkr@gmail.com"],
+	  "jason": ["jason", "jason@test.com"],
+	  "tom": ["tom", "tom@test.com"],
+	  "peter": ["peter", "peter@test.com"],
   };
 
-
-  // Demo only: print the form data onscreen as a formatted JSON object.
-  var dataContainer = document.getElementsByClassName('results__display')[0];
   
-  // Use `JSON.stringify()` to make the output valid, human-readable JSON.
-  dataContainer.textContent = JSON.stringify(data, null, "  ");
-  
-  // ...this is where we’d actually do something with the form data...
+  // Create array with names of people participating in Secret Santa
   var namesArr = [];
   for ( x in data ) {
   	namesArr.push(x);
   }
-  console.log("Array is " + namesArr);
 
+  // Function that randomly shuffles contents of array
   function shuffle(array) {
 	  var currentIndex = array.length, temporaryValue, randomIndex;
 
@@ -88,10 +64,41 @@ function handleFormSubmit(event) {
 	  return array;
 	};
 
+	// Call the shuffle function on namesArr
 	namesArr = shuffle(namesArr);
 
-	console.log("Array is now " + namesArr);
+	/**
+	 * Loop through the newly shuffled namesArr
+	 * Push the name of the next person in the array to the current person's object property
+	 * This ensures two people aren't paired together
+	 */
+	for (var i = 0; i < namesArr.length; i++) {
+		if (i === namesArr.length - 1) {
+			data[namesArr[i]].push(data[namesArr[0]][0]);
+		} else {
+			data[namesArr[i]].push(data[namesArr[i + 1]][0]);			
+		}
+	}
 
+	/**
+	 * Send email using EmailJS.com
+	 * data[y][1] = email of secret santa
+	 * data[y][2] = name of the person receiving gift
+	 */
+  for (y in data) {
+  	emailjs.send("default_service","secret_santa",{
+		  email: data[y][1], 
+		  giftie: data[y][2]
+		});
+  }
+  
+
+
+	// Demo only: print the form data onscreen as a formatted JSON object.
+  var dataContainer = document.getElementsByClassName('results__display')[0];
+  
+  // Use `JSON.stringify()` to make the output valid, human-readable JSON.
+  dataContainer.textContent = JSON.stringify(data, null, "  ");
 
 };
 
