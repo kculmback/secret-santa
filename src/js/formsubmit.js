62c9d7d46e9a9695d8316bec49ce739efea22ca6
Kasey Culmback <kasey@kcfeatures.com>
2016-12-13 17:51:30 -0800

    Multiple additions

--- src/js/formsubmit.js
+++ src/js/formsubmit.js
@@ -14,15 +14,17 @@
  * @param  {HTMLFormControlsCollection} elements  the form elements
  * @return {Object}                               form data as an object literal
  */
-function formToJSON(elements) {
+/*function formToJSON(elements) {
     return [].reduce.call(elements, function (data, element) {
-        if (isValidElement(element)) {
+        if (element.name.slice(0,4) === "name") {
         	data[element.name] = element.value;
         }
         
         return data;
     }, {});
-};
+	
+
+};*/
 
 
 /**
@@ -36,8 +38,25 @@ function handleFormSubmit(event) {
   event.preventDefault();
   
   // TODO: Call our function to get the form data.
-  var data = formToJSON(form.elements);
-  
+  // var data = formToJSON(form.elements);
+  /*var data = {};
+  $(form).find("input").each(function() {
+	    // The selector will match buttons; if you want to filter
+	    // them out, check `this.tagName` and `this.type`; see
+	    // below
+	    // var inputType = this.tagName.toUpperCase() === "INPUT" && this.type.toUpperCase();
+	    if (this.name.slice(0,4) === "name") {
+	    	data[this.value] = $(this).next().next().next().next().val();
+	    }
+	});*/
+  var data = {
+  	"kasey": "kasey@test.com",
+  "jason": "jason@jason.com",
+  "tom": "tom@test.com",
+  "peter": "peter@test.com",
+  };
+
+
   // Demo only: print the form data onscreen as a formatted JSON object.
   var dataContainer = document.getElementsByClassName('results__display')[0];
   
@@ -45,7 +64,35 @@ function handleFormSubmit(event) {
   dataContainer.textContent = JSON.stringify(data, null, "  ");
   
   // ...this is where we’d actually do something with the form data...
-  console.log(data);
+  var namesArr = [];
+  for ( x in data ) {
+  	namesArr.push(x);
+  }
+  console.log("Array is " + namesArr);
+
+  function shuffle(array) {
+	  var currentIndex = array.length, temporaryValue, randomIndex;
+
+	  // While there remain elements to shuffle...
+	  while (0 !== currentIndex) {
+
+	    // Pick a remaining element...
+	    randomIndex = Math.floor(Math.random() * currentIndex);
+	    currentIndex -= 1;
+
+	    // And swap it with the current element.
+	    temporaryValue = array[currentIndex];
+	    array[currentIndex] = array[randomIndex];
+	    array[randomIndex] = temporaryValue;
+	  }
+	  return array;
+	};
+
+	namesArr = shuffle(namesArr);
+
+	console.log("Array is now " + namesArr);
+
+
 };
 
 var form = document.getElementsByClassName('secretSantaForm')[0];
