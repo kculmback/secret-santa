ed685ccdc57c919f159592104541ca1dd299cfe2
Kasey Culmback <kasey@kcfeatures.com>
2016-12-14 18:12:30 -0800

    Write better comments and move array shuffle function out of event handler function

--- src/js/formsubmit.js
+++ src/js/formsubmit.js
@@ -1,12 +1,26 @@
 /**
- * Checks that an element has a non-empty `name` and `value` property.
- * @param  {Element} element  the element to check
- * @return {Bool}             true if the element is an input, false if not
+ * Function that will randomly shuffle an array
+ * Fisher-Yates (aka Knuth) Shuffle
+ * http://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
  */
- function isValidElement(element) {
-    return element.name && element.value;
-};
 
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+  return array;
+};
 
 /**
  * A handler function to prevent default submission and run our custom script.
@@ -21,11 +35,8 @@ function handleFormSubmit(event) {
   // Create object with names and emails of people participating
   var data = {};
   $(form).find("input").each(function() {
-	    // The selector will match buttons; if you want to filter
-	    // them out, check `this.tagName` and `this.type`; see
-	    // below
-	    // var inputType = this.tagName.toUpperCase() === "INPUT" && this.type.toUpperCase();
-	    
+	    // Check to see if input is a name or an email
+	    // Only fire if working with a name
 	    if (this.name.slice(0,4) === "name") {
 	    	data[this.value] = [$(this).val(), $(this).next().val()]
 	    }
@@ -38,41 +49,23 @@ function handleFormSubmit(event) {
 	  "peter": ["peter", "peter@test.com"],
   };*/
 
+  // Capture size of data object
   var objSize = Object.keys(data).length;
-  console.log(objSize);
-
   
   // Create array with names of people participating in Secret Santa
+  // This will make it easy to randomly assign people to one another
   var namesArr = [];
   for ( x in data ) {
   	namesArr.push(x);
   }
 
-  // Function that randomly shuffles contents of array
-  function shuffle(array) {
-	  var currentIndex = array.length, temporaryValue, randomIndex;
-
-	  // While there remain elements to shuffle...
-	  while (0 !== currentIndex) {
-
-	    // Pick a remaining element...
-	    randomIndex = Math.floor(Math.random() * currentIndex);
-	    currentIndex -= 1;
-
-	    // And swap it with the current element.
-	    temporaryValue = array[currentIndex];
-	    array[currentIndex] = array[randomIndex];
-	    array[randomIndex] = temporaryValue;
-	  }
-	  return array;
-	};
-
 	// Call the shuffle function on namesArr
 	namesArr = shuffle(namesArr);
 
 	/**
-	 * Loop through the newly shuffled namesArr
-	 * Push the name of the next person in the array to the current person's object property
+	 * Loop through the newly shuffled namesArr.
+	 * Assigns the next name in the array to the current name's property in the data object
+	 * If last name in the array, it will assign the first name to their property 
 	 * This ensures two people aren't paired together
 	 */
 	for (var i = 0; i < namesArr.length; i++) {
@@ -85,32 +78,46 @@ function handleFormSubmit(event) {
 
 	/**
 	 * Send email using EmailJS.com
+	 * data[y][0] = name of secret santa
 	 * data[y][1] = email of secret santa
 	 * data[y][2] = name of the person receiving gift
 	 */
 
+	// Change text of submit button while function runs
 	$(form).find(".submitBtn").text("Pairing...");
+
+	// Initialize variables to be used in for...in loop
 	var successArr = [];
 	var z = 0;
+
   for (y in data) {
   	emailjs.send("default_service","secret_santa",{
 		  santa: data[y][0],
 		  email: data[y][1], 
 		  giftie: data[y][2]
-		}).then(function() {
-			$(form).find(".submitBtn").text("Pair up!")
-		}, function(err) {
+		})
+		// After sending email perform this function if succeeds
+		.then(function() {
+			// Originally tried to push only the successful names to the succerArr using:
+			// successArr.push(" " + data[y][0]);
+			// However, this wouldn't work. Currenlt do so below.
+		}, 
+		// If there is an error, alert it to user
+		function(err) {
 			alert("Send email failed!\r\n Response:\n " + JSON.stringify(err));
-			$(form).find(".submitBtn").text("Pair up!")
 		});
+		// Push names of secret santas to successArr
+		// Later on will display these names
 		successArr.push(" " + data[y][0]);
+		// Increment counter
 		z++;
+		// Call back to perform once program is at end of the loop
 		if (z === objSize) {
 			var resultsContainer = document.getElementsByClassName('results')[0];
 			var dataContainer = document.getElementsByClassName('results__display')[0];
 			resultsContainer.style.display = "block";
 		  dataContainer.textContent = successArr;
-		  console.log(successArr);
+		  $(form).find(".submitBtn").text("Pair up!")
 		}
   }
   
