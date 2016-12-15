046d803e2bdc69f0bdf10d3e32bdb9bd6368856d
Kasey Culmback <kasey@kcfeatures.com>
2016-12-14 18:43:39 -0800

    Better comments, remove white space, fix remove section parent calls

--- src/js/form.js
+++ src/js/form.js
@@ -1,46 +1,41 @@
-	//define template
-	var template = $('#sections .section:first').clone();
+/**
+ * This script is in charge of adding and removing participatents from form 
+ * Relys on jQuery to clone Secret Santa form data
+ */
 
-	//define counter
-	var sectionsCount = 0;
+// Define template variable. 
+// This will contain the html required to create new participants
+var template = $('#sections .section:first').clone();
 
-	//add new section
-	$('body').on('click', '.addsection', function() {
+// Define counter
+var sectionsCount = 0;
 
-	  //increment
-	  sectionsCount++;
+// When user clicks .addsection button, adds new participant
+$('body').on('click', '.addsection', function() {
+  // Increment sectionsCount
+  sectionsCount++;
+  // Loop through each input
+  var section = template.clone().find(':input').each(function(){
+    // Set id to store the updated section number
+    var newId = this.id.slice(0, 4) + sectionsCount;
+    // Update id
+    this.id = newId;
+    // Update name
+    $(this).attr('name', newId);
+	}).end()
+  // Inject new section
+  .appendTo('#sections');
+  return false;
+});
 
-	  //loop through each input
-	  var section = template.clone().find(':input').each(function(){
-
-      //set id to store the updated section number
-      var newId = this.id.slice(0, 4) + sectionsCount;
-
-      //update for label
-      $(this).prev().attr('for', newId);
-
-      //update id
-      this.id = newId;
-
-      //update name
-      $(this).attr('name', newId);
-      
-  	}).end()
-
-
-
-    //inject new section
-    .appendTo('#sections');
+// When user clicks .remove button, removes that participant
+$('#sections').on('click', '.remove', function() {
+  // Fade out section
+  $(this).parent().parent().fadeOut(300, function(){
+    // Remove parent element (main section)
+    $(this).parent().empty();
     return false;
-	});
-	//remove section
-	$('#sections').on('click', '.remove', function() {
-    //fade out section
-    $(this).parent().fadeOut(300, function(){
-      //remove parent element (main section)
-      $(this).parent().parent().empty();
-      return false;
-    });
-    return false;
-	});
+  });
+  return false;
+});
 
