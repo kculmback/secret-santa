23acaf9d47f7293683d8b2cf8238dbd358f3e743
Kasey <kasey@kcfeatures.com>
2017-01-13 09:58:46 -0800

    Add better comments and clean up code

--- src/js/form.js
+++ src/js/form.js
@@ -7,32 +7,26 @@
 // This will contain the html required to create new participants
 var template = $('#sections .section:first').clone();
 
-// Define counter
 var sectionsCount = 0;
 
 // When user clicks .addsection button, adds new participant
 $('body').on('click', '.addsection', function() {
-  // Increment sectionsCount
+
   sectionsCount++;
+
   // Loop through each input
   var section = template.clone().find(':input').each(function(){
-    // Set id to store the updated section number
-    var newId = this.id.slice(0, 4) + sectionsCount;
-    // Update id
+    var newId = this.id.slice(0, -1) + sectionsCount;
     this.id = newId;
-    // Update name
     $(this).attr('name', newId);
 	}).end()
-  // Inject new section
   .appendTo('#sections');
   return false;
 });
 
 // When user clicks .remove button, removes that participant
 $('#sections').on('click', '.remove', function() {
-  // Fade out section
   $(this).parent().parent().fadeOut(300, function(){
-    // Remove parent element (main section)
     $(this).parent().empty();
     return false;
   });
--- src/js/formsubmit.js
+++ src/js/formsubmit.js
@@ -1,9 +1,42 @@
+// Main function for Secret Santa web app
+// Stores data from form in array and uses that data to pair participants
+function secretSantaFormSubmit(event) {
+
+	event.preventDefault();
+
+	$(submitBtn).text("Pairing...");
+	$(secretSantaForm).find(".results").css("display", "block");
+
+	var participants = [];
+
+	// Create an array for each participant within the participants array
+	// Ends up with a 2 dimensional array
+	$(secretSantaForm).find(".section").each(function() {
+		// var participant = [
+		// 	$(this).find(".nameInput").val(),
+		// 	$(this).find(".emailInput").val()
+		// ];
+
+		var participant = {
+			name: $(this).find(".nameInput").val(),
+			email: $(this).find(".emailInput").val()
+		};
+		participants.push(participant);
+	});
+	
+	participants = shuffle(participants);
+
+	participants.forEach(matchParticipants);
+
+	participants.forEach(emailParticipants);  
+};
+
+
 /**
  * Function that will randomly shuffle an array. 
  * Fisher-Yates (aka Knuth) Shuffle
  * http://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
  */
-
 function shuffle(array) {
 	var currentIndex = array.length, temporaryValue, randomIndex;
 
@@ -22,104 +55,45 @@ function shuffle(array) {
 	return array;
 };
 
+// Function to match participants with recipients 
+// Matches current participant in array with the next participant in array
+// Unless they are the last, in which case matches them with the first participant in array
+function matchParticipants (participant, index, array) {
+	return participant.recipient = (array[index + 1] || array[0]).name;
+};
 
-/**
- * A handler function to prevent default submission and run our custom script.
- * @param  {Event} event  the submit event triggered by the user
- * @return {void}
- */
-function handleFormSubmit(event) {
-
-	// Stop the form from submitting since we’re handling that with AJAX.
-	event.preventDefault();
-
-	// Create object with names and emails of people participating
-	var data = {};
-	$(secretSantaForm).find("input").each(function() {
-		// Check to see if input is a name or an email
-		// Only fire if working with a name
-		if (this.name.slice(0,4) === "name") {
-			data[this.value] = [$(this).val(), $(this).next().val()]
-		}
-	});
-
-
-	// Capture length of data object
-	var objSize = Object.keys(data).length;
-
-
-	// Create array with names of people participating in Secret Santa
-	// This will make it easy to randomly assign people to one another
-	var namesArr = [];
-	for ( x in data ) {
-		namesArr.push(x);
-	}
-	// Call the shuffle function on namesArr
-	namesArr = shuffle(namesArr);
-
-	/**
-	 * Loop through the newly shuffled namesArr.
-	 * Assigns the next name in the array to the current name's property in the data object
-	 * If last name in the array, it will assign the first name to their property 
-	 * This ensures two people aren't paired together
-	 */
-	for (var i = 0; i < namesArr.length; i++) {
-		if (i === namesArr.length - 1) {
-			data[namesArr[i]].push(data[namesArr[0]][0]);
-		} else {
-			data[namesArr[i]].push(data[namesArr[i + 1]][0]);			
-		}
-	}
-
-	/**
-	 * Send email using EmailJS.com
-	 * data[y][0] = name of secret santa
-	 * data[y][1] = email of secret santa
-	 * data[y][2] = name of the person receiving gift
-	 */
-
-	// Change text of submit button while function runs
-	$(secretSantaForm).find(".submitBtn").text("Pairing...");
-
-	// Initialize variables to be used in for...in loop
-	var successArr = [];
-	var z = 0;
-	for (y in data) {
-		emailjs.send("default_service","secret_santa",{
-			santa: data[y][0],
-			email: data[y][1],
-			giftie: data[y][2]
-		})
+// Function that uses emailjs API to send notification emails to secret santas
+function emailParticipants (participant, index, array) {
+	emailjs.send("default_service","secret_santa",{
+		santa: participant.name,
+		email: participant.email,
+		giftie: participant.recipient
+	})
 		// After sending email perform this function if succeeds
-		.then(function() {
-			// Originally tried to push only the successful names to the succerArr using:
-			// successArr.push(" " + data[y][0]);
-			// However, this wouldn't work. Currently doing so below.
+		.then(function(response) {
+			console.log(participant.name + ": SUCCESS", response);
+			$(resultsDisplay).append("<p>Successfully sent to: " + participant.name + "</p>");
+
+			if (index === array.length - 1) {
+				$(submitBtn).text("Pair up!");
+			}
 		}, 
 		// If there is an error, alert it to user
 		function(err) {
-			alert("Send email failed!\r\n Response:\n " + JSON.stringify(err));
+			$(resultsDisplay).append("<p>Uh oh! Unable to send email to: " + participant.name + "<br>Error: " + JSON.stringify(err) + "</p>");
+			console.log("FAILED", err);
+			if (index === array.length - 1) {
+				$(submitBtn).text("Pair up!");
+			}
 		});
-		// Push names of secret santas to successArr
-		// Later on will display these names
-		// Need to fix: will still push failed names to array
-		successArr.push(" " + data[y][0]);
-		// Increment counter
-		z++;
-		// Call back to perform once program is at end of the loop
-		if (z === objSize) {
-			// Show the names from successArr and change the submit button back to normal
-			$(secretSantaForm).find(".results").css("display", "block");
-			$(secretSantaForm).find(".results__display").text(successArr);
-			$(secretSantaForm).find(".submitBtn").text("Pair up!");
-		}
-	}
-  
 };
 
-// Find the Secret Santa form in the dom and assign it to form variable
+// Store elements that are used multiple times in functions as variables
 var secretSantaForm = document.getElementsByClassName('secretSantaForm')[0];
+var resultsDisplay = document.getElementsByClassName('results-display')[0];
+var submitBtn = document.getElementsByClassName('submitBtn')[0];
 
-// When the submit button is clicked for our form fire the function that
-// assigns secret santas and sends emails
-secretSantaForm.addEventListener('submit', handleFormSubmit);
+// Trigger secretSantaFormSubmit function when form is submitted
+secretSantaForm.addEventListener('submit', secretSantaFormSubmit);
+
+
